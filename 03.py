358c6457eade97310470633f9951987b45b3a3ba
yellow77 <yellowloveraul@gmail.com>
2019-07-16 14:37:21 +0800

    03.py練習

---
+++ 03.py
@@ -0,0 +1,17 @@
+temperature = 30
+
+if temperature > 30 :
+    print("it is hot")
+else:
+    print("it is not hot")
+
+
+name = input("put your name here ")
+n = len(name)
+
+if n <= 3 :
+    print("should be more than 3 charecters")
+elif n > 50 :
+    print("error")
+else:
+    print("name looks good")
